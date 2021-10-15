57ad4f526baf6460f8aecc385c719396bcd6f712
cogwizzle <joseph.fehrman@gmail.com>
2021-10-14 20:56:55 -0400

    Updating blog list component to pull the text of the blogs as well.

--- src/component/blog-list.js
+++ src/component/blog-list.js
@@ -1,16 +1,51 @@
+import { md } from '../utility/markdown.js';
+
+const getBlogList = async () => {
+  try {
+    const response = await fetch(`/api/blog/index.json`);
+    const data = await response.json();
+    return await Promise.all(
+      data.map(async (blog) => {
+        try {
+          const response = await fetch(`/api/blog/${blog.location}.md`);
+          const body = await response.text();
+          const preview = md(`${body.substring(0, 200)}...`);
+          return {
+            ...blog,
+            body: preview,
+          };
+        } catch (error) {
+          throw new Error(error);
+        }
+      })
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 export class BlogList extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.loading = true;
-    fetch('/blog/index.json')
-      .then((res) => res.json())
+    getBlogList()
       .then((data) => {
-        this.loading = false;
         this.data = data;
+        this.loading = false;
         this.render();
-      });
+      })
+      .then((render) => this.render())
+      .catch((error) => this.showErrorState());
+
     this.render();
+  }
+
+  showErrorState() {
+    const shadowRoot = this.shadowRoot;
+    const template = document.createElement('template');
+    template.innerHTML = 'Error loading blog entries.';
+    shadowRoot.innerHTML = '';
+    shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   showLoader() {
@@ -33,12 +68,22 @@ export class BlogList extends HTMLElement {
     const shadowRoot = this.shadowRoot;
     const template = document.createElement('template');
     template.innerHTML = `
+      <style>
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        a:visited {
+          color: inherit;
+        }
+      </style>
       ${this.data.map(
         (entry) =>
-          `<a href="/blog/#${entry.location}">
+          `<a href="/blog/index.html#${entry.location}">
             <div class="entry">
-              <h2>${entry.title}</h2>
               <p>${entry.date} - ${entry.author}</p>
+              <p>${entry.body}</p>
             </div>
           </a>`
       )}
