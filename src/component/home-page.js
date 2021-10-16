78a9ac4a3277ad279b2e80adf920022d68f04c7b
cogwizzle <joseph.fehrman@gmail.com>
2021-10-15 22:01:33 -0400

    Creating home page component.

---
+++ src/component/home-page.js
@@ -0,0 +1,27 @@
+import { TitlePageLayout } from './title-page-layout.js';
+import { BlogList } from './blog-list.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <cw-title-page-layout>
+    <cw-blog-list></cw-blog-list>
+  </cw-title-page-layout>
+`;
+
+export class HomePage extends HTMLElement {
+  constructor() {
+    super();
+    if (customElements.get('cw-title-page-layout') === undefined) {
+      customElements.define('cw-title-page-layout', TitlePageLayout);
+    }
+    if (customElements.get('cw-blog-list') === undefined) {
+      customElements.define('cw-blog-list', BlogList);
+    }
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = '';
+    this.appendChild(template.content.cloneNode(true));
+  }
+}
