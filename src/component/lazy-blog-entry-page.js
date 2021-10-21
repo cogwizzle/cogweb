697a4ebe7fe90f408d35445a35bbf77f88d89752
cogwizzle <joseph.fehrman@gmail.com>
2021-10-21 19:33:10 -0400

    Removing dead code.

--- src/component/lazy-blog-entry-page.js
+++
@@ -1,7 +0,0 @@
-import { CreateLazyLoader } from './lazy-loader-element.js';
-
-export const LazyBlogEntryPage = CreateLazyLoader({
-  componentName: 'BlogEntryPage',
-  path: './blog-entry-page.js',
-  tagName: 'cw-blog-entry-page',
-});
