2a8abe73897f0b48928f3351b5ab2ba87af2bb81
cogwizzle <joseph.fehrman@gmail.com>
2021-10-21 19:24:01 -0400

    Updating to use ez-hash-router 0.0.3 so that we can do lazy loading more easily.

--- src/component/blog-router.js
+++ src/component/blog-router.js
@@ -1,7 +1,7 @@
-import { RouterElement } from 'https://unpkg.com/ez-hash-router@0.0.2/index.js';
+import { RouterElement } from 'https://unpkg.com/ez-hash-router@0.0.3/index.js';
 import { HomePage } from './home-page.js';
 import { LazyBlogEntryPage } from './lazy-blog-entry-page.js';
-
+import { loadingObservable } from './page-loading-bar.js';
 export class BlogRouter extends RouterElement {
   constructor() {
     super();
@@ -17,11 +17,40 @@ export class BlogRouter extends RouterElement {
     this.routes = [
       {
         path: 'blog/{entry}',
-        component: 'cw-lazy-blog-entry-page',
+        go: async ({ variables }) => {
+          if (customElements.get('cw-blog-entry-page') !== undefined) {
+            return `<cw-blog-entry-page
+              ${variables
+                .map(({ name, value }) => {
+                  return `${name}="${value}"`;
+                })
+                .join('\n')}
+            />`;
+          }
+
+          loadingObservable.pushLoadingState('lazy-blog-entry-page');
+          try {
+            const { BlogEntryPage } = await import('./blog-entry-page.js');
+            if (!customElements.get('cw-blog-entry-page')) {
+              customElements.define('cw-blog-entry-page', BlogEntryPage);
+            }
+            return `<cw-blog-entry-page
+              ${variables
+                .map(({ name, value }) => {
+                  return `${name}="${value}"`;
+                })
+                .join('\n')}
+            />`;
+          } catch (e) {
+            console.log(e);
+          } finally {
+            loadingObservable.resolveLoadingState('lazy-blog-entry-page');
+          }
+        },
       },
       {
         path: '',
-        component: 'cw-home-page',
+        go: () => '<cw-home-page></cw-home-page>',
       },
     ];
     super.connectedCallback();
