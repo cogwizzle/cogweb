00f36bd048062898de1bcdf5c512cc216d522254
cogwizzle <joseph.fehrman@gmail.com>
2021-10-25 11:13:11 -0400

    Adding types to the default toast.

--- src/component/toast.js
+++ src/component/toast.js
@@ -141,9 +141,32 @@ export const toast = (message, options = {}) => {
   toastObservable.createToast(message, options);
 };
 
+const determineColors = (type) => {
+  switch (type) {
+    case 'danger':
+      return {
+        'background-color': '#f8d7da',
+        border: 'solid 1px #f5c2c7',
+        color: '#842029',
+      };
+    case 'warning':
+      return {
+        'background-color': '#fff3cd',
+        border: 'solid 1px #ffecb5',
+        color: '#664d03',
+      };
+    default:
+      return {
+        'background-color': '#fff',
+        border: 'none',
+        color: '#636464',
+      };
+  }
+};
+
 export class ToastElement extends HTMLElement {
   static get observedAttributes() {
-    return ['key'];
+    return ['key', 'type'];
   }
 
   constructor() {
@@ -163,13 +186,24 @@ export class ToastElement extends HTMLElement {
     this.setAttribute('key', value);
   }
 
+  get type() {
+    return this.getAttribute('type');
+  }
+
+  set type(value) {
+    this.type = value;
+  }
+
   render() {
     const template = document.createElement('template');
+    const colors = determineColors(this.getAttribute('type'));
     template.innerHTML = `
       <style>
         :host {
           position: relative;
-          background-color: #fff;
+          background-color: ${colors['background-color']};
+          border: ${colors.border};
+          color: ${colors.color};
           border-radius: 5px;
           padding: 10px;
           box-shadow: 0px 0px 5px #000;
@@ -201,7 +235,7 @@ export class ToastElement extends HTMLElement {
   }
 }
 
-customElements.define('cw-default-toast', ToastElement);
+customElements.define('ez-default-toast', ToastElement);
 
 export class ToastWrapperElement extends HTMLElement {
   static get observedAttributes() {
@@ -243,16 +277,17 @@ export class ToastWrapperElement extends HTMLElement {
   }
 
   _createToastElements(acc, toast) {
-    const tagName = this.getAttribute('tag-name') || 'cw-default-toast';
+    const tagName = this.getAttribute('tag-name') || 'ez-default-toast';
     const toastElement = document.createElement(tagName);
     toastElement.key = toast.message;
+    toastElement.setAttribute('type', toast.options.type);
     toastElement.innerHTML = toast.message;
     acc[toast.options.position].push(toastElement);
     return acc;
   }
 
   _initialRender() {
-    const tagName = this.getAttribute('tag-name') || 'cw-default-toast';
+    const tagName = this.getAttribute('tag-name') || 'ez-default-toast';
     this.shadowRoot.innerHTML = '';
     this.shadowRoot.appendChild(
       toastGlobalTemplate(tagName).content.cloneNode(true)
